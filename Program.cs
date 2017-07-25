33b5d4cb30c8053cbee6852f7e32ffa6d60d4ad1
Adam Frisby <AdamFrisby@users.noreply.github.com>
2017-07-26 03:54:51 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace AutoDoxyBrane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectName = "Your API";
+            string projectDesc = "Your API for autocompletion";
+            string shortcode = "yourapi";
+            string rootClass = "rootClass";
+            string rootLibName = "YourAPI";
+
+            string dir = "T:\\trunk\\documentation\\scripting\\xml";
+            string output = "T:\\trunk\\documentation\\scripting\\yourapi.lua";
+
+            string wrapper =
+                "local interpreter = {\r\n  name = \"%projname%\",\r\n  description = \"%projdesc%\",\r\n  api = { \"baselib\", \"%shortcode%\" },\r\n  fattachdebug = function(self) DebuggerAttachDefault() end,\r\n  skipcompile = true,\r\n}\r\n\r\nlocal api = %APIDEFINITION%\r\n\r\n\r\nreturn {\r\n  name = \"%projname%\",\r\n  description = \"%projdesc%\",\r\n  onRegister = function(self)\r\n    ide:AddAPI(\"lua\", \"%shortcode%\", api)\r\n    ide:AddInterpreter(\"%shortcode%\", interpreter)\r\n  end,\r\n  onUnRegister = function(self)\r\n    ide:RemoveAPI(\"lua\", \"%shortcode%\")\r\n    ide:RemoveInterpreter(\"%shortcode%\")\r\n  end,\r\n}";
+
+            var files = Directory.GetFiles(dir, "*.xml");
+
+            List<string> members = new List<string>();
+
+            foreach (var file in files)
+            {
+                string fn = Path.GetFileName(file);
+                if (fn.StartsWith("class_") || fn.StartsWith("struct_"))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(Doxygen));
+                    using (TextReader reader = new StringReader(Sanitise(File.ReadAllText(file))))
+                    {
+                        Doxygen result = (Doxygen)serializer.Deserialize(reader);
+
+                        members.Add(GenerateTable(result, rootClass, rootLibName));
+                    }
+                }
+            }
+            
+            var contents = wrapper
+                            .Replace("%APIDEFINITION%", "{ \n" + string.Concat(members) + "}")
+                            .Replace("%projname%", projectName)
+                            .Replace("%projdesc%", projectDesc)
+                            .Replace("%shortcode%", shortcode);
+
+            File.WriteAllText(output, contents);
+        }
+
+        private static string Sanitise(string file)
+        {
+            Regex r = new Regex("(<ref .*\">)", RegexOptions.Compiled);
+
+            var matches = r.Matches(file);
+            foreach (Match match in matches)
+            {
+                file = file.Replace(match.Value, "");
+            }
+
+            return file.Replace("</ref>", "");
+        }
+
+        private static string GenerateTable(Doxygen doxy, string rootClass, string rootLibName)
+        {
+            if ((doxy.Compounddef.Kind == "class" || doxy.Compounddef.Kind == "struct") && doxy.Compounddef.Prot == "public")
+            {
+                List<string> children = new List<string>();
+
+                string name = doxy.Compounddef.Compoundname;
+
+                var sections = doxy.Compounddef.Sectiondef;
+                foreach (var section in sections)
+                {
+                    foreach (var member in section.Memberdef)
+                    {
+                        if (member.Prot != "public")
+                            continue;
+
+                        if (member.Kind == "property")
+                        {
+                            children.Add("\n\t\t\t" + member.Name + " = { type = \"value\", valuetype=\"" + member.Type + "\"},");
+                        }
+                        else if (member.Kind == "variable") // consts
+                        {
+                            string type = member.Definition;
+
+                            if (string.IsNullOrEmpty(type) || (!string.IsNullOrEmpty(member.Type) && !member.Type.Contains(" ")))
+                                type = member.Type;
+
+                            if (type.Contains(" "))
+                                type = type.Split(' ')[1];
+
+                            children.Add("\n\t\t\t" + member.Name + " = { type = \"value\", valuetype=\"" + type + "\"},");
+                        }
+                        else if (member.Kind == "function")
+                        {
+                            if (member.Name.StartsWith("operator"))
+                                continue;
+
+                            string type = member.Type;
+                            if (type == "void")
+                                type = "nil";
+
+                            children.Add("\n\t\t\t" + member.Name + " = { type=\"function\", " +
+                                         "args =\"" + member.Argsstring + "\", " +
+                                         "returns =\"(" + type + ")\", " +
+                                         "valuetype =\"" + type + "\", " +
+                                         "},");
+                        }
+                    }
+                }
+                
+                string t = "class";
+
+                if (name == rootClass)
+                {
+                    t = "lib";
+                    name = rootLibName;
+                }
+
+                return "\t" + name + " = {\n\t\ttype=\"" + t + "\",\n\t\tchilds= {" + string.Join("", children) + "\n\t\t}\n\t},\n";
+            }
+
+            return "";
+        }
+    }
+}
---
+++ XmlDefs.cs
@@ -0,0 +1,176 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace AutoDoxyBrane
+{
+    [XmlRoot(ElementName = "ref")]
+    public class Ref
+    {
+        [XmlAttribute(AttributeName = "refid")]
+        public string Refid { get; set; }
+        [XmlAttribute(AttributeName = "kindref")]
+        public string Kindref { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "type")]
+    public class Type
+    {
+        [XmlElement(ElementName = "ref")]
+        public Ref Ref { get; set; }
+    }
+
+    [XmlRoot(ElementName = "location")]
+    public class Location
+    {
+        [XmlAttribute(AttributeName = "file")]
+        public string File { get; set; }
+        [XmlAttribute(AttributeName = "line")]
+        public string Line { get; set; }
+        [XmlAttribute(AttributeName = "column")]
+        public string Column { get; set; }
+        [XmlAttribute(AttributeName = "bodyfile")]
+        public string Bodyfile { get; set; }
+        [XmlAttribute(AttributeName = "bodystart")]
+        public string Bodystart { get; set; }
+        [XmlAttribute(AttributeName = "bodyend")]
+        public string Bodyend { get; set; }
+    }
+
+    [XmlRoot(ElementName = "memberdef")]
+    public class Memberdef
+    {
+        [XmlElement(ElementName = "type")]
+        public string Type { get; set; }
+        [XmlElement(ElementName = "definition")]
+        public string Definition { get; set; }
+        [XmlElement(ElementName = "argsstring")]
+        public string Argsstring { get; set; }
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        //[XmlElement(ElementName = "briefdescription")]
+        //public string Briefdescription { get; set; }
+        //[XmlElement(ElementName = "detaileddescription")]
+        //public string Detaileddescription { get; set; }
+        //[XmlElement(ElementName = "inbodydescription")]
+        //public string Inbodydescription { get; set; }
+        [XmlElement(ElementName = "location")]
+        public Location Location { get; set; }
+        [XmlAttribute(AttributeName = "kind")]
+        public string Kind { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "prot")]
+        public string Prot { get; set; }
+        [XmlAttribute(AttributeName = "static")]
+        public string Static { get; set; }
+        [XmlAttribute(AttributeName = "mutable")]
+        public string Mutable { get; set; }
+        [XmlAttribute(AttributeName = "readable")]
+        public string Readable { get; set; }
+        [XmlAttribute(AttributeName = "writable")]
+        public string Writable { get; set; }
+        [XmlAttribute(AttributeName = "gettable")]
+        public string Gettable { get; set; }
+        [XmlAttribute(AttributeName = "privategettable")]
+        public string Privategettable { get; set; }
+        [XmlAttribute(AttributeName = "protectedgettable")]
+        public string Protectedgettable { get; set; }
+        [XmlAttribute(AttributeName = "settable")]
+        public string Settable { get; set; }
+        [XmlAttribute(AttributeName = "privatesettable")]
+        public string Privatesettable { get; set; }
+        [XmlAttribute(AttributeName = "protectedsettable")]
+        public string Protectedsettable { get; set; }
+        [XmlAttribute(AttributeName = "const")]
+        public string Const { get; set; }
+        [XmlAttribute(AttributeName = "explicit")]
+        public string Explicit { get; set; }
+        [XmlAttribute(AttributeName = "inline")]
+        public string Inline { get; set; }
+        [XmlAttribute(AttributeName = "virt")]
+        public string Virt { get; set; }
+        [XmlElement(ElementName = "param")]
+        public List<Param> Param { get; set; }
+    }
+
+    [XmlRoot(ElementName = "sectiondef")]
+    public class Sectiondef
+    {
+        [XmlElement(ElementName = "memberdef")]
+        public List<Memberdef> Memberdef { get; set; }
+        [XmlAttribute(AttributeName = "kind")]
+        public string Kind { get; set; }
+    }
+
+    [XmlRoot(ElementName = "param")]
+    public class Param
+    {
+        [XmlElement(ElementName = "type")]
+        public string Type { get; set; }
+        [XmlElement(ElementName = "declname")]
+        public string Declname { get; set; }
+    }
+
+    [XmlRoot(ElementName = "member")]
+    public class Member
+    {
+        [XmlElement(ElementName = "scope")]
+        public string Scope { get; set; }
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlAttribute(AttributeName = "refid")]
+        public string Refid { get; set; }
+        [XmlAttribute(AttributeName = "prot")]
+        public string Prot { get; set; }
+        [XmlAttribute(AttributeName = "virt")]
+        public string Virt { get; set; }
+    }
+
+    [XmlRoot(ElementName = "listofallmembers")]
+    public class Listofallmembers
+    {
+        [XmlElement(ElementName = "member")]
+        public List<Member> Member { get; set; }
+    }
+
+    [XmlRoot(ElementName = "compounddef")]
+    public class Compounddef
+    {
+        [XmlElement(ElementName = "compoundname")]
+        public string Compoundname { get; set; }
+        [XmlElement(ElementName = "sectiondef")]
+        public List<Sectiondef> Sectiondef { get; set; }
+        [XmlElement(ElementName = "briefdescription")]
+        public string Briefdescription { get; set; }
+        [XmlElement(ElementName = "detaileddescription")]
+        public string Detaileddescription { get; set; }
+        [XmlElement(ElementName = "location")]
+        public Location Location { get; set; }
+        [XmlElement(ElementName = "listofallmembers")]
+        public Listofallmembers Listofallmembers { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "kind")]
+        public string Kind { get; set; }
+        [XmlAttribute(AttributeName = "language")]
+        public string Language { get; set; }
+        [XmlAttribute(AttributeName = "prot")]
+        public string Prot { get; set; }
+    }
+
+    [XmlRoot(ElementName = "doxygen")]
+    public class Doxygen
+    {
+        [XmlElement(ElementName = "compounddef")]
+        public Compounddef Compounddef { get; set; }
+        [XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsi { get; set; }
+        [XmlAttribute(AttributeName = "noNamespaceSchemaLocation", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string NoNamespaceSchemaLocation { get; set; }
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+    }
+
+}
